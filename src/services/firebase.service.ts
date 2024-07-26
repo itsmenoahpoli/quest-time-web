4ac2f2aee049cb72cea20b1fdabdc8212f62f40c
Patrick Noah Policarpio <patrickpolicarpio08@gmail.com>
2024-07-26 08:30:31 +0800

    create firebase service

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from 'firebase/firestore'
 import { getEnv } from "~/config/env.config";
 
 const firebaseConfig = {
@@ -11,9 +12,52 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp)
 
 console.log(firebaseApp);
 
 export const FirebaseService = {
-  //
+  getFirestore: () => db,
+  getCollection: (collectionName: string) => collection(db, collectionName),
+
+  async createDocument(collectionName: string, data: any) {
+    const docRef = await addDoc(collection(db, collectionName), data);
+    return {
+      id: docRef.id,
+      ...data
+    };
+  },
+
+  async getDocumentById(collectionName: string, documentId: string) {
+    const docRef = doc(db, collectionName, documentId);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return {
+        id: docSnap.id,
+        ...docSnap.data()
+      };
+    } else {
+      return null;
+    }
+  },
+
+  async updateDocumentById(collectionName: string, documentId: string, data: any) {
+    const docRef = doc(db, collectionName, documentId);
+    await updateDoc(docRef, data);
+
+    return {
+      id: documentId,
+      ...data
+    };
+  },
+
+  async deleteDocumentById(collectionName: string, documentId: string) {
+    const docRef = doc(db, collectionName, documentId);
+    await deleteDoc(docRef);
+
+    return {
+      id: documentId
+    };
+  }
 };
